a99a0b19f1f14b81104ef42a460f7bd15424ecc7
Tofik Gashaw <gashawtofik@gmail.com>
2022-04-18 06:16:57 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -8,40 +8,74 @@
  * specifiers contained into fmt
  * Return: length of the formatted output string
  */
-int _printf(const char *format, ...)
-{
-	int (*pfunc)(va_list, flags_t *);
-	const char *p;
-	va_list arguments;
-	flags_t flags = {0, 0, 0};
+int _printf(char* format,...) 
+{ 
+    char *traverse; 
+    unsigned int i; 
+    char *s; 
 
-	register int count = 0;
+    //Module 1: Initializing Myprintf's arguments 
+    va_list arg; 
+    va_start(arg, format); 
 
-	va_start(arguments, format);
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (p = format; *p; p++)
-	{
-		if (*p == '%')
-		{
-			p++;
-			if (*p == '%')
-			{
-				count += _putchar('%');
-				continue;
-			}
-			while (get_flag(*p, &flags))
-				p++;
-			pfunc = get_print(*p);
-			count += (pfunc)
-				? pfunc(arguments, &flags)
-				: _printf("%%%c", *p);
-		} else
-			count += _putchar(*p);
-	}
-	_putchar(-1);
-	va_end(arguments);
-	return (count);
+    for(traverse = format; *traverse != '\0'; traverse++) 
+    { 
+        while( *traverse != '%' ) 
+        { 
+            _putchar(*traverse);
+            traverse++; 
+        } 
+
+        traverse++; 
+
+        //Module 2: Fetching and executing arguments
+        switch(*traverse) 
+        { 
+            case 'c' : i = va_arg(arg,int);     //Fetch char argument
+                        _putchar(i);
+                        break; 
+
+            case '%' : i = va_arg(arg,int);         //Fetch Decimal/Integer argument
+                        if(i<0) 
+                        { 
+                            i = -i;
+                            _putchar('-'); 
+                        } 
+                        _puts(convert(i,10));
+                        break; 
+
+            case 'o': i = va_arg(arg,unsigned int); //Fetch Octal representation
+                        _puts(convert(i,8));
+                        break; 
+
+            case 's': s = va_arg(arg,char *);       //Fetch string
+                        _puts(s); 
+                        break; 
+
+            case 'x': i = va_arg(arg,unsigned int); //Fetch Hexadecimal representation
+                        _puts(convert(i,16));
+                        break; 
+        }   
+    } 
+
+    //Module 3: Closing argument list to necessary clean-up
+    va_end(arg); 
+} 
+
+char *convert(unsigned int num, int base) 
+{ 
+    static char Representation[]= "0123456789ABCDEF";
+    static char buffer[50]; 
+    char *ptr; 
+
+    ptr = &buffer[49]; 
+    *ptr = '\0'; 
+
+    do 
+    { 
+        *--ptr = Representation[num%base]; 
+        num /= base; 
+    }while(num != 0); 
+
+    return(ptr); 
 }
