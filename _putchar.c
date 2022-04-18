2fbb4aef82d234386f813374bd098c59e7206aef
fikir54 <fikir.page@gmail.com>
2022-04-18 16:19:16 +0300

    Print F

--- _putchar.c
+++ _putchar.c
@@ -1,5 +1,4 @@
 #include <unistd.h>
-#include "main.h"
 
 /**
  * _putchar - writes the character c to stdout
@@ -10,28 +9,6 @@
  */
 int _putchar(char c)
 {
-	return (buffer(c));
-}
-
-/**
- * buffer - Save the character in a buffer
- * @c: Character
- *
- * Return: 1
- **/
-int buffer(char c)
-{
-	static char buffering[1024];
-	static int i;
-
-	if (c == -1 || i == 1024)
-	{
-		write(1, buffering, i);
-		i = 0;
-	}
-
-	if (c != -1)
-		buffering[i++] = c;
-
+	write(1, &c, 1);
 	return (1);
 }
