23c491360ef100722510f860c4eb0e99d7b242e5
fikir54 <fikir.page@gmail.com>
2022-04-19 20:07:48 +0300

    Main file

--- main.h
+++ main.h
@@ -1,15 +1,33 @@
-#ifndef PRINTF_H
-#define PRINTF_H
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
 #include <stdarg.h>
-#include <limits.h>
 
-void _sprintf(const char *str);
+int _putchar(char c);
+int print_int(va_list arg);
+int print_unsigned(va_list arg);
 int _printf(const char *format, ...);
-void _count(int *ptr, char *str);
+int print_char(va_list arg);
+int print_str(va_list arg);
+int print_percent(void);
+void print_binary(unsigned int n, unsigned int* printed);
+int print_unsignedToBinary(va_list arg);
+int print_oct(va_list arg);
+int print_unsignedIntToHex(unsigned int num, char _case);
+int print_hex_base(va_list arg, char _case);
+int print_hex(va_list arg);
+int print_HEX(va_list arg);
+int print_STR (va_list arg);
 
+/**
+ * struct identifierStruct - structure definition of a printTypeStruct
+ * @indentifier: type
+ * @printer: function to print
+ */
+typedef struct identifierStruct
+{
+char *indentifier;
+int (*printer)(va_list);
+} identifierStruct;
 
 #endif
