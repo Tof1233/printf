4ce4f6735fb4b211781866bce3dbb370c4d603d6
fikir54 <fikir.page@gmail.com>
2022-04-19 22:37:27 +0300

    Main File

--- _putchar.c
+++ _putchar.c
@@ -6,24 +6,5 @@
  */
 int _putchar(char c)
 {
-	static int contador;
-	static char buffer[1024];
-
-	if (c == -1)
-	{
-		contador = 0;
-		return (0);
-	}
-	if (c == -2 || contador == 1024)
-	{
-		write(1, buffer, contador);
-		contador = 0;
-	}
-	if (c != -1 && c != -2)
-	{
-		buffer[contador] = c;
-		contador++;
-		return (1);
-	}
-	return (0);
+	return (write(1, &c, 1));
 }
